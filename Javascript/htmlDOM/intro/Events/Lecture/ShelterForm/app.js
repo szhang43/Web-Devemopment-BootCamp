55ddfdf6a87c8726b5a3a6ec3fa5d239f1f87db2
sophiez <sophiez.89@outlook.com>
2023-08-20 18:54:56 -0700

    Clean up code for shelter forms

--- Javascript/htmlDOM/intro/Events/Lecture/ShelterForm/app.js
+++ Javascript/htmlDOM/intro/Events/Lecture/ShelterForm/app.js
@@ -1,18 +1,43 @@
 const form = document.querySelector("#shelterForm"); 
-
 const formInfo = document.querySelectorAll("#catInfo");
 
+function createButton(){
+    const remove = document.createElement("button");
+    remove.innerText = "X";
+    remove.classList.add("removeBtn");
+    return remove;
+}
 
-form.addEventListener("submit", function(e){
-    e.preventDefault(); // prevent page refresh
+function deleteRow(row){
+    const deleteBtn = document.querySelectorAll(".removeBtn"); 
+    for(let btn of deleteBtn){
+        btn.addEventListener("click", function(e){
+            row.remove();
+        })
+    }
+}
+
+function createRow(){
     const row = document.createElement("tr");  // create a new row
+    const deleteBtn = createButton();
+
     for(let info of formInfo){ // loop through responses
         const element = document.createElement("td"); // create an individual cell
         element.innerText = info.value; // set the cell value
-        
-        document.querySelector("#cats").append(element); // add cell to table
+        row.append(element); // add cell to table
         info.value = " ";
     }
-    document.querySelector("#cats").append(row); // add a new row
+    row.append(deleteBtn); 
+    return row;
+}
+
+form.addEventListener("submit", function(e){
+    e.preventDefault(); // prevent page refresh
+    const newRow = createRow();
+
+    document.querySelector("#cats").append(newRow); // add the row
+    deleteRow(newRow);
 });
 
+
+// e.stopPropagation stops bubbling on events
