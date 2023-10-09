3923c4d85ed2568bd590835859b3a8784a49a698
sophiez <sophiez.89@outlook.com>
2023-10-08 18:07:24 -0700

    Exporting Modules

---
+++ 25_Node_Intro/exportModules/app.js
@@ -0,0 +1,3 @@
+const math = require('./math');
+
+console.log(math.add(9, 8)); 
---
+++ 25_Node_Intro/exportModules/math.js
@@ -0,0 +1,22 @@
+const add = (x, y) =>{
+    return x + y;
+}
+
+const PI = 3.141592653;
+
+const square = x => x * x; 
+
+const math = {
+    add : add, 
+    PI : PI, 
+    square : square
+}
+
+
+// module.exports = math;
+
+
+// module.exports.add = add; 
+// module.exports.PI = PI; 
+// module.exports.square = square;
+
