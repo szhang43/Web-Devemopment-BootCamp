40f07020799a178d1b2b96ac376d0bfa96759fed
sophiez <sophiez.89@outlook.com>
2023-10-08 22:05:39 -0700

    Renamed file

---
+++ 26_Modules_and_NPM/shelter/index.js
@@ -0,0 +1,8 @@
+const blue = require("./blue");
+const sadie = require("./sadie");
+const jannet = require("./jannet");
+
+const cats = [blue, sadie, jannet];
+
+
+module.exports = cats;
---
+++ 26_Modules_and_NPM/shelter/jannet.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name : "Jannet", 
+    color : "orange"
+};
