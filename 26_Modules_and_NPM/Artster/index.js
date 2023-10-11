a6d2f5a453b19cac1370818843fb75496e3ded9b
sophiez <sophiez.89@outlook.com>
2023-10-11 15:47:20 -0700

    Removing node module

--- 26_Modules_and_NPM/Artster/index.js
+++
@@ -1,9 +0,0 @@
-var figlet = require("figlet"); 
-var color = require("colors");
-
-figlet("Brina", function(e, data){
-    if(e){
-        console.log("Uhh oh, something went wrong...")
-    }
-    console.log(data.rainbow);
-})
