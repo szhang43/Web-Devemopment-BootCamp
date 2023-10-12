93c4d301cb1cf518ba535bd9056788c5211b7950
sophiez <sophiez.89@outlook.com>
2023-10-11 17:35:06 -0700

    language guesser game

--- 26_Modules_and_NPM/languageGuesser/index.js
+++ 26_Modules_and_NPM/languageGuesser/index.js
@@ -1,26 +1,33 @@
-import {franc} from 'franc';
-import langs from "langs";
-import process from "process"; 
-import colors from "colors"
+// Import necessary libraries
+import { franc } from 'franc'; // Language detection library
+import langs from 'langs'; // Language information library
+import process from 'process'; // Node.js process library
+import colors from 'colors'; // Text colorization library
 
-const input = process.argv[2]; 
+// Get the user input from the command line
+const input = process.argv[2];
 
+// Use 'franc' to detect the language of the user's input
 const code = franc(`${input}`);
 
-try{
-    if(code == "und"){ // if code is undetermined
-        console.log(`Your input is too short or invalid!`.red);
+try {
+  if (code == 'und') {
+    // If 'franc' couldn't detect the language or the input is too short, display a message in red.
+    console.log('Your input is too short or invalid!'.red);
+  } else {
+    // If 'franc' detected a language code, use 'langs' to get the language name.
+    const language = langs.where('3', code);
+
+    if (typeof language !== 'undefined') {
+      // If the language is defined, display the best guess in blue.
+      const result = language.name.blue;
+      console.log(`Our best guess is: ${result}!`);
+    } else {
+      // If the language is not defined, display a message in red.
+      console.log(`We weren't able to guess the language...`.red);
     }
-    else{
-        const language = langs.where("3", code);
-        if(typeof language !== "undefined"){
-            const result = language.name.blue;
-            console.log(`Our best guess is: ${result}!`);
-        }
-        else{
-            console.log(`We weren't able to guess the language...`.red);
-        }
-    }
-}catch(error){
-    console.log(`There was an error: ${error}`.red);
+  }
+} catch (error) {
+  // If there is an error, display the error message in red.
+  console.log(`There was an error: ${error}`.red);
 }
