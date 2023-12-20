8f5e8b6180d9f3c986fa59c395afa23a61683c71
sophiez <sophiez.89@outlook.com>
2023-12-19 23:29:48 -0800

    Completed CRUD functionality of YelpCamp

--- 29-YelpCamp/app.js
+++ 29-YelpCamp/app.js
@@ -1,6 +1,7 @@
 /* Express app packages */
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 
 /* Mongoose Connection & Model */
 const Campground = require("./models/campground");
@@ -19,16 +20,63 @@ const path = require("path");
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+/* app.use */
+app.use(express.urlencoded({ extended : true })); // Parse req.body from express 
+app.use(methodOverride("_method"));
+
 /* CampApp Routes */
-app.get('/', (req, res) => {
-    res.render("home");
+
+// Home page - Display all available camps
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    // console.log(campgrounds);
+    res.render('campgrounds/index', { campgrounds });
 });
 
-app.get('/makecamp', (req, res) => {
-    const camp = new Campground({
-        title : "Nature Nova"
-    });
+// New Form Page - Fill out details to be sent to DB
+app.get('/campgrounds/new', (req, res) =>{
+    res.render('campgrounds/new');
 })
+
+//Post Request - Adds the new camp information to DB
+app.post('/campgrounds', async(req, res) => {
+    const { title, location } = req.body;
+    const newCamp = new Campground({
+        title : title, 
+        location : location
+    });
+    await newCamp.save();
+    res.redirect('campgrounds');
+});
+
+// Edit Route - finds the id of camp to update
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render('campgrounds/edit', { camp })
+});
+
+app.put('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params; 
+    const updateCamp = await Campground.findByIdAndUpdate(id, { ...req.body.camp });
+    res.redirect('/campgrounds');
+})
+
+// Show page - Display the single clicked camp 
+app.get('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params; 
+    const camp = await Campground.findById(id);
+    // console.log(camp);
+    res.render('campgrounds/show', { camp });
+});
+
+
+app.delete('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params;
+    const deleteCamp = await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
 /* App is listening on PORT 3000 */
 app.listen(3000, () => {
     console.log("Listening on PORT 3000")
--- 29-YelpCamp/seeds/index.js
+++ 29-YelpCamp/seeds/index.js
@@ -1,6 +1,5 @@
 /* Mongoose Connection & Model */
 const Campground = require("../models/campground");
-
 const mongoose = require('mongoose'); 
 mongoose.connect('mongodb://localhost:27017/YelpCamp')
     .then(() => {
@@ -10,11 +9,28 @@ mongoose.connect('mongodb://localhost:27017/YelpCamp')
         console.log("There was an error...", error);
     })
 
+/* Seed Imports */
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
 
-const seedDB = async() => {
-    await Campground.deleteMany({});
-    const redwood = new Campground({title : "RedWood Camp"});
-    await redwood.save();
+const sample = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-seedDB();
+/* seedDB Generating Camp Data */
+const seedDB = async() => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const randNum = Math.floor(Math.random() * 1000);
+        const camps = new Campground({
+            title : `${sample(descriptors)} ${sample(places)}`,
+            location : `${cities[randNum].city}, ${cities[randNum].state}`,
+        })
+        await camps.save();
+    }
+}
+
+seedDB()
+    .then(() => {
+        mongoose.connection.close();
+    })
