79dd101144e0c5411499e48fd2fdb01a6cbd6a36
sophiez <sophiez.89@outlook.com>
2024-01-06 18:44:49 -0800

    Incorporate some error handling to yelpcamp

--- 29-YelpCamp/app.js
+++ 29-YelpCamp/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 const ejsmate = require('ejs-mate');
+const wrapAsync = require('./utilities/wrapAsync');
+const appError = require('./utilities/errorClass');
 app.engine('ejs', ejsmate);
 
 /* Mongoose Connection & Model */
@@ -29,13 +31,10 @@ app.use(methodOverride("_method"));
 /* CampApp Routes */
 
 // Home page - Display all available camps
-app.get('/campgrounds', async(req, res) => {
+app.get('/campgrounds', wrapAsync(async(req, res) => {
     const campgrounds = await Campground.find({});
-    // console.log("************************")
-    // console.log(campgrounds);
-    // console.log("************************")
     res.render('campgrounds/index', { campgrounds });
-});
+}));
 
 // New Form Page - Fill out details to be sent to DB
 app.get('/campgrounds/new', (req, res) =>{
@@ -43,40 +42,47 @@ app.get('/campgrounds/new', (req, res) =>{
 })
 
 //Post Request - Adds the new camp information to DB
-app.post('/campgrounds', async(req, res) => {
+app.post('/campgrounds', wrapAsync(async(req, res) => {
     const campground = new Campground(req.body.camp);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}));
 
 // Edit Route - finds the id of camp to update
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', wrapAsync(async(req, res) => {
     const { id } = req.params;
     const camp = await Campground.findById(id);
     res.render('campgrounds/edit', { camp })
-});
+}));
 
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', wrapAsync(async(req, res) => {
     const { id } = req.params; 
     const updateCamp = await Campground.findByIdAndUpdate(id, { ...req.body.camp });
     res.redirect('/campgrounds');
-})
+}));
 
 // Show page - Display the single clicked camp 
-app.get('/campgrounds/:id', async(req, res) => {
+app.get('/campgrounds/:id', wrapAsync(async(req, res) => {
     const { id } = req.params; 
     const camp = await Campground.findById(id);
     // console.log(camp);
     res.render('campgrounds/show', { camp });
-});
+}));
 
 
-app.delete('/campgrounds/:id', async(req, res) => {
+app.delete('/campgrounds/:id', wrapAsync(async(req, res) => {
     const { id } = req.params;
     const deleteCamp = await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}));
 
+app.use((err, req, res, next) => {
+    const { status=500, message="Something went wrong!" } = err;
+    res.status(status).send(message);
+})
+app.use((err,req, res, next) => {
+    res.send("There was an error...")
+})
 /* App is listening on PORT 3000 */
 app.listen(3000, () => {
     console.log("Listening on PORT 3000")
---
+++ 29-YelpCamp/utilities/wrapAsync.js
@@ -0,0 +1,8 @@
+const wrapAsync = (fn) => {
+    return function(req, res, next){
+        fn(req, res, next).catch(error => next(error));
+    };
+};
+
+
+module.exports = wrapAsync;
